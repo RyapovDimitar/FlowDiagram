ff9be27d34be15c2285a59fee39a3f830fb6d6f3
Andrei Rosca <andi23rosca@gmail.com>
2017-01-11 16:36:01 +0100

    Added the menus

--- FlowDiagramApplication/FlowDiagramApplication/Form1.cs
+++ FlowDiagramApplication/FlowDiagramApplication/Form1.cs
@@ -23,5 +23,58 @@ namespace FlowDiagramApplication
         {
             fl.Components = new List<Component>();
         }
+
+        private void btnProperties_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void btnPump_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void btnSink_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void btnSplitter_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void btnAdjSplitter_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void btnMerger_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void btnPipeline_Click(object sender, EventArgs e)
+        {
+            ToolstripButtonCheck(sender);
+        }
+
+        private void ToolstripButtonCheck(object sender)
+        {
+            // This method makes the other buttons in the toolstrip uncheck when one button is selected
+            foreach (ToolStripButton item in ((ToolStripButton)sender).GetCurrentParent().Items)
+            {
+                if (item == sender) item.Checked = true;
+                if ((item != null) && (item != sender))
+                {
+                    item.Checked = false;
+                }
+            }
+        }
     }
 }
